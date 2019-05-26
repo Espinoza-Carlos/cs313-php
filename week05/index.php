86a2bba6b7a4689c5ad2d6ee500fba0ad1a4795a
carlos-espinoza.io <46772397+Espinoza-Carlos@users.noreply.github.com>
2019-05-26 02:08:07 -0700

    Update index.php

--- week05/index.php
+++ week05/index.php
@@ -22,7 +22,13 @@
   <br><br>
   <input type="submit">
 </form>
-<p><b>Food Item...................Category name.............Description..................Food Unit...................Count </b>      </p>
+<table>
+    <tr>
+        <th>Food Item</th>
+        <th>Category Name</th> <th>Description</th> <th>Food Unit</th>
+        <th>Count</th>
+    </tr>
+<!--<p><b>Food Item...................Category name.............Description..................Food Unit...................Count </b>      </p>-->
 <?php
 require 'db.php';
 //Local or Heroku
@@ -36,11 +42,15 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     } 
     foreach($allRows as $r) 
     {
-     echo'<div class="row">'. '<div class="column">'.$r['food_item'].  '</div>'. '<div class="column">'  .$r['category_name']. '</div>'. '<div class="column">' .$r['description']. '</div>'. '<div class="column">'. $r['food_unit']. '</div>'. '<div class="column">' . $r['count']. '</div>'.'</div>'.'<br>' ;        
+     /*echo'<div class="row">'. '<div class="column">'.$r['food_item'].  '</div>'. '<div class="column">'  .$r['category_name']. '</div>'. '<div class="column">' .$r['description']. '</div>'. '<div class="column">'. $r['food_unit']. '</div>'. '<div class="column">' . $r['count']. '</div>'.'</div>'.'<br>' ;*/
+    /*
+        echo'<table>'. '<thead>'. '<tr>'. '<th>'. 'Food Item'. '</th>'. '<th>'. 'Category Name'. '</th>' . '<th>'. 'Description'. '</th>'. '<th>'. 'Food Unit'. '</th>'. '<th>'. 'Count'. '</th>'. '</tr>'.  '</thead>'. '<tr>'. '<td>' .$r['food_item'].   '</td>'. '<td>'  .$r['category_name']. '</td>'. '<td>' .$r['description']. '</td>'. '<td>'. $r['food_unit']. '</td>'. '<td>' . $r['count']. '</td>'.'</tr>'. '</table>'. '<br>'  ;            */
+        echo'<tr>'. '<td>' .$r['food_item'].   '</td>'. '<td>'  .$r['category_name']. '</td>'. '<td>' .$r['description']. '</td>'. '<td>'. $r['food_unit']. '</td>'. '<td>' . $r['count']. '</td>'.'</tr>';
     }
 
     } 
 ?>
+    </table>    
 </div>        
 <hr>    
 <div>    
