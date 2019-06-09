b5ce791fb8c0310870bc2b66f3c8b1115d636165
carlos-espinoza.io <46772397+Espinoza-Carlos@users.noreply.github.com>
2019-06-09 06:04:54 -0700

    Update myDb.sql

--- week07/myDb.sql
+++ week07/myDb.sql
@@ -1,3 +1,10 @@
+create table ta07_users
+(    
+	id serial not null primary key,
+	login varchar(40) not null UNIQUE,
+    password varchar(200) not null
+);
+
 CREATE TABLE foodStorage (
     id                  SERIAL PRIMARY KEY,
     Food_item       varchar(80),   
